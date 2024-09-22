37797aaf27ef6af723c1ad870a9f18dc840a30d8
sz318 <sz318@duke.edu>
2024-09-21 20:21:06 -0400

    Did assignment 40

--- 039_tests_subseq/maxSeq.c
+++ 039_tests_subseq/maxSeq.c
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 
 size_t maxSeq(int * array, size_t n) {
-  size_t tempLen = 0, maxLen = 0;
+  if (n == 0)
+    return 0;
+  size_t tempLen = 1, maxLen = 1;
   for (size_t i = 1; i < n; ++i) {
     if (array[i] > array[i - 1]) {
       ++tempLen;
