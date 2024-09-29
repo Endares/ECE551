a836f45a0f41b88b76a2d5f32d68c997456cb1d4
sz318 <sz318@duke.edu>
2024-09-29 13:45:31 -0400

    Did assignment 50

--- 050_break_encr/breaker.c
+++ 050_break_encr/breaker.c
@@ -42,10 +42,6 @@ int main(int argc, char * argv[]) {
   }
   //  printf("Most frequent letter is %c.\n", (char)(mostFrequent + 'a'));
   int key = (26 + mostFrequent + ('a' - 'e')) % 26;
-  if (key < 0 || key >= 26) {
-    fprintf(stderr, "Invalid key = %d\n", key);
-    exit(EXIT_FAILURE);
-  }
 
   fprintf(stdout, "%d\n", key);
   //printRes(key, f);
