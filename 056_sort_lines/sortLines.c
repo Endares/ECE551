b056f5132536e0deda465a0fd29b9d28f50faa19
sz318 <sz318@duke.edu>
2024-10-06 18:23:11 -0400

    Did assignment 56

--- 056_sort_lines/sortLines.c
+++ 056_sort_lines/sortLines.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,8 +15,58 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void readSortPrintData(FILE * f) {
+  char ** lines = NULL;
+  char * curr = NULL;
+  size_t linecap;
+  size_t i = 0;
+  while (getline(&curr, &linecap, f) >= 0) {
+    char ** temp = realloc(lines, (i + 1) * sizeof(*lines));
+    if (temp == NULL) {
+      fprintf(stderr, "Memory allocation error\n");
+      free(lines);
+      exit(EXIT_FAILURE);
+    }
+    lines = temp;
+    // lines = realloc(lines, (i + 1) * sizeof(*lines));
+    lines[i] = curr;
+    curr = NULL;
+    ++i;
+  }
+  free(curr);
+  sortData(lines, i);
+  for (size_t j = 0; j < i; ++j) {
+    printf("%s", lines[j]);  // getline() 's string has a '\n' before '\0'
+    free(lines[j]);
+  }
+  free(lines);
+}
+
 int main(int argc, char ** argv) {
   //WRITE YOUR CODE HERE!
+  FILE * f;
+  if (argc == 1) {
+    f = stdin;
+    readSortPrintData(f);
+    if (fclose(f) != 0) {
+      fprintf(stderr, "Error closing file: %s\n", strerror(errno));
+      exit(EXIT_FAILURE);
+    }
+  }
+  else {
+    for (int i = 1; i < argc; ++i) {
+      f = fopen(argv[i], "r");
+      if (f == NULL) {
+        fprintf(stderr, "Error opening file: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+      }
+      readSortPrintData(f);
+      if (fclose(f) != 0) {
+        fprintf(stderr, "Error closing file: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
 
   return EXIT_SUCCESS;
 }
