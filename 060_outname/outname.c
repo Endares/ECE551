1075d79cd0373213e12b39f2446a9766fd95b71f
sz318 <sz318@duke.edu>
2024-10-07 22:29:55 -0400

    Did assignment 60

--- 060_outname/outname.c
+++ 060_outname/outname.c
@@ -1,8 +1,16 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  const char * suffix = ".counts";
+  unsigned len = strlen(inputName) + strlen(suffix) +
+                 1;  // one space for '\0' because strlen() doesn't count for '\0'
+  char * outputName = malloc(len * sizeof(*outputName));  // malloc space of 8 chars
+  //fill in a string with the outputName
+  snprintf(outputName, len, "%s%s", inputName, suffix);
+  return outputName;
 }
