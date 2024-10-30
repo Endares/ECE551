8cf4db2f006ccc0d1b46e35fa9a0c24b2d8720ae
sz318 <sz318@duke.edu>
2024-10-30 14:34:08 -0400

    Did assignment 82

--- 082_sort_cpp/sortLines.cpp
+++ 082_sort_cpp/sortLines.cpp
@@ -1,36 +1,43 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <istream>
 #include <string>
 #include <vector>
 using namespace std;
 
+void readSortPrint(istream & is) {
+  vector<string> lines;
+  string curr;
+  // read from standard input
+  while (getline(is, curr)) {
+    lines.push_back(curr);
+    // push_back will make a copy of curr, so int the next round, curr won't be covered.
+  }
+
+  if (!is.eof() && !is) {
+    cout << "Failed to read." << endl;
+    exit(EXIT_FAILURE);
+  }
+
+  sort(lines.begin(), lines.end());
+  for (auto s : lines) {
+    cout << s << endl;
+  }
+}
+
 int main(int argc, char * argv[]) {
   if (argc == 1) {
-    vector<string> lines;
-    string curr;
-    // read from standard input
-    while (getline(cin, curr)) {
-      lines.push_back(curr);
-      // push_back will make a copy of curr, so int the next round, curr won't be covered.
-    }
-    sort(lines.begin(), lines.end());
-    for (auto s : lines) {
-      cout << s << endl;
-    }
+    readSortPrint(cin);
   }
   else {
     for (int i = 1; i < argc; ++i) {
-      vector<string> lines;
       ifstream f(argv[i]);  // open input file as a ifstream
-      string curr;
-      while (getline(f, curr)) {
-        lines.push_back(curr);
+      if (!f) {
+        cerr << "Failed to open the file" << argv[i] << endl;
+        exit(EXIT_FAILURE);
       }
-      sort(lines.begin(), lines.end());
-      for (auto s : lines) {
-        cout << s << endl;
-      }
+      readSortPrint(f);
       f.close();
     }
   }
