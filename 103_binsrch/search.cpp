f4ed9bec9fbb7d5f8acb8d21310aba4004d9fb89
sz318 <sz318@duke.edu>
2024-11-19 23:41:32 -0500

    Did Assignment 103

--- 103_binsrch/search.cpp
+++ 103_binsrch/search.cpp
@@ -1,14 +1,15 @@
 #include "function.h"
 
 int binarySearchForZero(Function<int, int> * f, int low, int high) {
-  while (low < high) {
+  // base case: low >= high - 1, only <= 1 elements, directly return
+  while (low < high - 1) {
     int mid = (low + high) / 2;
     int x = f->invoke(mid);
     if (x == 0) {
       return mid;
     }
     else if (x < 0) {
-      low = mid + 1;
+      low = mid;
     }
     else {
       high = mid;
