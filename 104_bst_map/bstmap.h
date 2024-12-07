e698cdfb9ceee074d656f014cc20bb0a4ef8f1e6
sz318 <sz318@duke.edu>
2024-12-07 17:32:13 -0500

    Did Assignment 104

---
+++ 104_bst_map/bstmap.h
@@ -0,0 +1,130 @@
+#ifndef _BSTMAP_H__
+#define _BSTMAP_H__
+
+#include <cstdlib>
+
+#include "map.h"
+// sorted according to key, not value!
+template<typename K, typename V>
+class BstMap : public Map<K, V> {
+ private:
+  class Node {
+   public:
+    K key;
+    V value;
+    Node * left;
+    Node * right;
+
+   public:
+    Node() {}
+    Node(const K & key, const V & value) :
+        key(key), value(value), left(NULL), right(NULL) {}
+  };
+  Node * root;
+
+ public:
+  BstMap() : root(NULL) {}  // don't forget to initialize
+  virtual void add(const K & key, const V & value) {
+    Node ** temp = find(key);
+    // doubly ptr to make sure new Node is inserted into the BST.
+
+    if (*temp) {
+      (*temp)->value = value;
+    }
+    else {
+      temp = &root;
+      while (*temp) {
+        if (key > (*temp)->key) {
+          temp = &(*temp)->right;
+        }
+        else {
+          temp = &(*temp)->left;
+        }
+      }
+      *temp = new Node(key, value);
+    }
+  }
+  virtual const V & lookup(const K & key) const throw(std::invalid_argument) {
+    Node ** temp = find(key);
+    if (!temp) {
+      throw std::invalid_argument("Key not found!");
+    }
+    else {
+      return (*temp)->value;
+    }
+  }
+  virtual void remove(const K & key) {
+    Node ** temp = find(key);
+    if (!(*temp)) {
+      throw std::invalid_argument("Key not found!");
+    }
+    else {
+      if (!(*temp)->left && !(*temp)->right) {
+        delete (*temp);
+        *temp = NULL;
+      }
+      else if (!(*temp)->left) {
+        Node * substitute = findRightMin(*temp);
+        (*temp)->key = substitute->key;
+        (*temp)->value = substitute->value;
+        delete substitute;
+        *temp = NULL;
+      }
+      else {
+        Node * substitute = findLeftMax(*temp);
+        (*temp)->key = substitute->key;
+        (*temp)->value = substitute->value;
+        delete substitute;
+        *temp = NULL;
+        // set *temp to NULL, in case of duplicated delete when destruction
+      }
+    }
+  }
+
+  Node * findRightMin(Node * cur) const {
+    Node * temp = cur;
+    temp = temp->right;
+    while (temp && temp->left) {
+      temp = temp->left;
+    }
+    return temp;
+  }
+
+  Node * findLeftMax(Node * cur) const {
+    Node * temp = cur;
+    temp = temp->left;
+    while (temp && temp->right) {
+      temp = temp->right;
+    }
+    return temp;
+  }
+
+  Node ** find(const K & key) const {
+    Node ** temp = const_cast<Node **>(&root);
+    while (*temp) {
+      if (key > (*temp)->key) {
+        temp = &(*temp)->right;
+      }
+      else if (key < (*temp)->key) {
+        temp = &(*temp)->left;
+      }
+      else {
+        return temp;
+      }
+    }
+    return temp;
+  }
+  virtual ~BstMap<K, V>() {
+    destroy(root);
+    root = NULL;
+  }
+  void destroy(Node * cur) {
+    if (cur) {
+      destroy(cur->left);
+      destroy(cur->right);
+      delete cur;
+    }
+  }
+};
+
+#endif
---
+++ 104_bst_map/test.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+#include "bstmap.h"
+
+int main(void) {
+  BstMap<int, int> map;
+  std::cout << "0" << std::endl;
+  map.add(60, 1);
+  std::cout << "1" << std::endl;
+  map.add(19, 2);
+  map.add(93, 3);
+  map.add(4, 4);
+  map.add(25, 5);
+  map.add(84, 6);
+  map.add(1, 7);
+  map.add(11, 8);
+  map.add(21, 9);
+  map.add(35, 10);
+  map.add(70, 11);
+  map.add(86, 12);
+  std::cout << "\n";
+  int ans1 = map.lookup(60);
+  int ans2 = map.lookup(35);
+  map.remove(19);
+  std::cout << "\n";
+  std::cout << "ans1 should be 1 and =" << ans1 << "\n";
+  std::cout << "ans2 should be 10 and =" << ans2 << "\n";
+  return EXIT_SUCCESS;
+}
