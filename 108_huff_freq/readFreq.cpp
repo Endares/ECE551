e1b3f465b642f66d1bb6d2f671b4ca4886d6b03e
sz318 <sz318@duke.edu>
2024-12-08 22:56:44 -0500

    Did assignment 108

--- 108_huff_freq/readFreq.cpp
+++ 108_huff_freq/readFreq.cpp
@@ -1,9 +1,13 @@
-#include <stdio.h>
-#include <cstdlib>
 #include "readFreq.h"
 
+#include <stdio.h>
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
 void printSym(std::ostream & s, unsigned sym) {
-  if (sym > 256 ) {
+  if (sym > 256) {
     s << "INV";
   }
   else if (sym == 256) {
@@ -21,4 +25,33 @@ void printSym(std::ostream & s, unsigned sym) {
 }
 uint64_t * readFrequencies(const char * fname) {
   //WRITE ME!
+  std::ifstream f(fname);
+  if (!f) {
+    std::cerr << "Failed to open the file " << fname << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  // dynamic allocation, initialize to 0 by using Value Initialization.
+  // default initialization: uint64_t* freqArray = new uint64_t[NUMCHARS], will lead to undefined values.
+  uint64_t * freqArray = new uint64_t[NUMCHARS]();
+
+  char c;
+  while (f.get(c)) {  // read character by character
+    freqArray[static_cast<unsigned char>(c)]++;
+  }
+
+  // eof
+  if (f.eof()) {
+    ++freqArray[256];
+  }
+  else if (f.fail()) {
+    std::cout << "File reading failed (not EOF)." << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  else {
+    std::cout << "Unknown error occurred." << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  f.close();
+  return freqArray;
 }
--- 108_huff_freq/readFreq.h
+++ 108_huff_freq/readFreq.h
@@ -1,8 +1,12 @@
 #ifndef __READ_FREQ_H__
 #define __READ_FREQ_H__
-#include <stdint.h>
 #include <assert.h>
+#include <stdint.h>
+
 #include <iostream>
+
+// ASCII: 256 characters, and an extra one for eof
+#define NUMCHARS 257
 
 uint64_t * readFrequencies(const char * fname);
 void printSym(std::ostream & s, unsigned sym);
