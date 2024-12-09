8fa61e2d0593f0b3461b3e1e2e52dbb57ef93242
sz318 <sz318@duke.edu>
2024-12-09 00:11:48 -0500

    Did assignment 111

--- 111_huff_all/compress.cpp
+++ 111_huff_all/compress.cpp
@@ -74,11 +74,14 @@ int main(int argc, char ** argv) {
   //hint 2: you can look at the main from the previous tester for 90% of this
   uint64_t * freqArray = readFrequencies(argv[1]);
   Node * root = buildTree(freqArray);
-  delete[] freqArray;
   std::map<unsigned, BitString> theMap;
   // BitString for root is empty
-  root->buildMap(BitString(), theMap);
+
+  BitString empty;
+  root->buildMap(empty, theMap);
+  // root->buildMap(BitString(), theMap);
   writeCompressedOutput(argv[1], argv[2], theMap);
+  delete[] freqArray;
   // need to deallocate root by hand, because root was created by new in buildTree.cpp
   delete root;
   // destructor of root will recursively destroy the whole tree
