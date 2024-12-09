ff4f86b0e4a47c4d8a15b44bbfe8ac4c880f319a
sz318 <sz318@duke.edu>
2024-12-08 23:13:02 -0500

    Did assignment 109

--- 109_huff_tree/buildTree.cpp
+++ 109_huff_tree/buildTree.cpp
@@ -1,5 +1,26 @@
 #include "node.h"
+#include "readFreq.h"
 
-Node *  buildTree(uint64_t * counts) {
+priority_queue_t makePQ(uint64_t * counts) {
+  priority_queue_t pq;
+  for (size_t i = 0; i < NUMCHARS; ++i) {
+    if (counts[i] != 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+  return pq;
+}
+
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t pq = makePQ(counts);
+  while (pq.size() > 1) {
+    // smallest: right child; second smallest: left child
+    Node * r = pq.top();
+    pq.pop();
+    Node * l = pq.top();
+    pq.pop();
+    pq.push(new Node(l, r));
+  }
+  return pq.top();
+}
