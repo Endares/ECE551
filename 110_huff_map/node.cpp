7eef796d86fb4f448fe12eb61d08ea3af0cb4462
sz318 <sz318@duke.edu>
2024-12-08 23:24:44 -0500

    Did assignment 110

--- 110_huff_map/node.cpp
+++ 110_huff_map/node.cpp
@@ -1,5 +1,17 @@
 #include "node.h"
-void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
+/* build map of all leaf Nodes under Node(*this) recursively */
+void Node::buildMap(BitString b, std::map<unsigned, BitString> & theMap) {
   //WRITE ME!
+  // leaf node
+  if (sym != NO_SYM) {
+    assert(!left && !right);
+    theMap[sym] = b;
+  }
+  // not leaf node, must have 2 children
+  else {
+    assert(left);
+    left->buildMap(b.plusZero(), theMap);
+    assert(right);
+    right->buildMap(b.plusOne(), theMap);
+  }
 }
-
